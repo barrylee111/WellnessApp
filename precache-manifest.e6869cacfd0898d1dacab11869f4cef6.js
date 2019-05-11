f81903b1f2ee3b35d9aff1d17c35c0ffbbdab14d
barrylee111 <barrylee115@gmail.com>
2019-05-10 22:02:17 -0600

    Updates

---
+++ precache-manifest.e6869cacfd0898d1dacab11869f4cef6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a4cac552238262eb3ccdf327d8fcdcc",
+    "url": "/wellnessapp/index.html"
+  },
+  {
+    "revision": "00ca24f476720803773a",
+    "url": "/wellnessapp/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/wellnessapp/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "00ca24f476720803773a",
+    "url": "/wellnessapp/static/js/main.938b7408.chunk.js"
+  },
+  {
+    "revision": "421d6621edb2d3fa35ac",
+    "url": "/wellnessapp/static/js/runtime~main.acf0e5a3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wellnessapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
